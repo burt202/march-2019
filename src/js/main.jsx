8113f307d3486807608c3ae1551f0aae366a654c
Aaron Burtnyk <burt202@gmail.com>
2018-04-23 20:34:12 +0100

    Add Where component

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,7 +1,7 @@
 const React = require("react")
 const createReactClass = require("create-react-class")
 
-const GoogleMap = require("./map")
+const Where = require("./where")
 
 require("../app.css")
 
@@ -34,20 +34,7 @@ const Main = createReactClass({
         </div>
         <div className="body">
           <p>Welcome to our little space on the internet where you can find out all about our special day. We aim to keep this page as up to date as we can, so do keep checking back!</p>
-          <h1>Where</h1>
-          <p>If you haven&apos;t already guessed, we will be getting married in Bristol! The ceremony will take place at the Bristol Register Office and the reception will take place at the Mshed. See below for details on both venues. There is approx a 10-15 min walk between the two venues.</p>
-          <h3>Bristol Register Office</h3>
-          <p>Address: The Old Council House, Corn St, Bristol BS1 1JG</p>
-          <h3>MShed</h3>
-          <p>Address: Princes Wharf, Wapping Rd, Bristol BS1 4RN</p>
-          <p><a target="_blank" rel="noopener noreferrer" href="https://www.bristolmuseums.org.uk/m-shed/">https://www.bristolmuseums.org.uk/m-shed/</a></p>
-          <GoogleMap
-            api={this.state.googleApi}
-            zoom={15}
-            lat={51.4509504}
-            lng={-2.5958109}
-            markers={[{label: "MShed", lat: 51.4474901, lng: -2.5981828}, {label: "Bristol Register Office", lat: 51.4548455, lng: -2.5933012}]}
-          />
+          <Where api={this.state.googleApi} />
           <p className="contact">contact@aaronandlaylah.co.uk</p>
         </div>
       </div>
---
+++ src/js/where.jsx
@@ -0,0 +1,36 @@
+const React = require("react")
+const createReactClass = require("create-react-class")
+const PropTypes = require("prop-types")
+
+const GoogleMap = require("./map")
+
+const Where = createReactClass({
+  displayName: "Where",
+
+  propTypes: {
+    api: PropTypes.object,
+  },
+
+  render() {
+    return (
+      <div>
+        <h1>Where</h1>
+        <p>If you haven&apos;t already guessed, we will be getting married in Bristol! The ceremony will take place at the Bristol Register Office and the reception will take place at the Mshed. See below for details on both venues. There is approx a 10-15 min walk between the two venues.</p>
+        <h3>Bristol Register Office</h3>
+        <p>Address: The Old Council House, Corn St, Bristol BS1 1JG</p>
+        <h3>MShed</h3>
+        <p>Address: Princes Wharf, Wapping Rd, Bristol BS1 4RN</p>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://www.bristolmuseums.org.uk/m-shed/">https://www.bristolmuseums.org.uk/m-shed/</a></p>
+        <GoogleMap
+          api={this.props.api}
+          zoom={15}
+          lat={51.4509504}
+          lng={-2.5958109}
+          markers={[{label: "MShed", lat: 51.4474901, lng: -2.5981828}, {label: "Bristol Register Office", lat: 51.4548455, lng: -2.5933012}]}
+        />
+      </div>
+    )
+  },
+})
+
+module.exports = Where
