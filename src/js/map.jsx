c6199d1c974d2ca717eb95d08297204bb5052505
Aaron Burtnyk <burt202@gmail.com>
2018-04-23 22:39:38 +0100

    Add icon and multi colour marker support to maps

--- src/js/map.jsx
+++ src/js/map.jsx
@@ -2,6 +2,12 @@ const React = require("react")
 const createReactClass = require("create-react-class")
 const PropTypes = require("prop-types")
 
+const markerMap = {
+  red: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+  green: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+  blue: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+}
+
 const GoogleMap = createReactClass({
   displayName: "GoogleMap",
 
@@ -26,18 +32,21 @@ const GoogleMap = createReactClass({
     const markers = this.props.markers || []
 
     markers.forEach(function(m) {
-      const infowindow = new api.maps.InfoWindow({
-        content: m.label,
-      })
-
       const marker = new api.maps.Marker({
         position: new api.maps.LatLng(m.lat, m.lng),
         map: this.map,
+        icon: m.icon || (markerMap[m.colour] || markerMap.red),
       })
 
-      api.maps.event.addListener(marker, "click", function() {
-        infowindow.open(this.map, marker)
-      })
+      if (m.label) {
+        const infowindow = new api.maps.InfoWindow({
+          content: m.label,
+        })
+
+        api.maps.event.addListener(marker, "click", function() {
+          infowindow.open(this.map, marker)
+        })
+      }
     }.bind(this))
   },
 
