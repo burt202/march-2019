7d87c9ae92d20e7523d6d8f5b38b5806e207302e
Aaron Burtnyk <burt202@gmail.com>
2018-08-20 21:37:34 +0100

    Update venue info

--- src/js/where.jsx
+++ src/js/where.jsx
@@ -15,12 +15,13 @@ const Where = createReactClass({
     return (
       <div>
         <h1>Where</h1>
-        <p>If you haven&apos;t already guessed, we will be getting married in Bristol! The ceremony will take place at the Bristol Register Office and the reception will take place at the Mshed. See below for details on both venues. There is approx a 10-15 min walk between the two venues.</p>
+        <p>If you haven&apos;t already guessed, we will be getting married in Bristol! The ceremony will take place at the Bristol Register Office and the reception will take place at the Mshed. See below for details on both venues.</p>
         <h3>Bristol Register Office</h3>
-        <p>Ceremony @ 2.15pm</p>
+        <p>Ceremony @ 2.15pm but arrive in good time to be seated before bride arrival</p>
         <p>Address: The Old Council House, Corn St, Bristol BS1 1JG</p>
         <h3>MShed</h3>
-        <p>From 3pm, evening entertainment @ 7.30pm</p>
+        <p>From 4pm - wedding breakfast @ 4.30pm, evening entertainment @ 7.30pm</p>
+        <p>NOTE: cash bar</p>
         <p>Address: Princes Wharf, Wapping Rd, Bristol BS1 4RN</p>
         <p><a target="_blank" rel="noopener noreferrer" href="https://www.bristolmuseums.org.uk/m-shed/">https://www.bristolmuseums.org.uk/m-shed/</a></p>
         <GoogleMap
@@ -30,6 +31,7 @@ const Where = createReactClass({
           lng={-2.5958109}
           markers={[{label: "MShed", lat: 51.4474901, lng: -2.5981828}, {label: "Bristol Register Office", lat: 51.4548455, lng: -2.5933012}]}
         />
+        <p>We have booked a Bristol ferry to transport everyone between the venues. So whilst we&apos;re away getting some photographs done, you will be getting a tour of the harbour by water, and yes, there will be a cash bar onboard!</p>
       </div>
     )
   },
