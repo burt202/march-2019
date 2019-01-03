b223f68c5e588b2d9f0a3fbfcc4758c4f0530b8d
Aaron Burtnyk <burt202@gmail.com>
2019-01-03 23:09:47 +0000

    Add photo uploader

---
+++ src/js/upload.jsx
@@ -0,0 +1,134 @@
+const React = require("react")
+const createReactClass = require("create-react-class")
+
+const LAMBDA_URL = "https://msny951f6i.execute-api.us-east-2.amazonaws.com/default/getSignedUrl"
+
+const Upload = createReactClass({
+  displayName: "Upload",
+
+  getInitialState: function() {
+    return {
+      uploaderName: "",
+      hover: false,
+      uploads: null,
+      error: null,
+    }
+  },
+
+  onInputChange: function(e) {
+    this.setState({uploaderName: e.target.value, error: null})
+  },
+
+  onDragEnter: function(e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    this.setState({hover: true})
+  },
+
+  onDragLeave: function(e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    this.setState({hover: false})
+  },
+
+  getPreSignedUrl: function(file) {
+    const fileName = file.name.split(".").slice(0, -1).join(".")
+
+    return fetch(`${LAMBDA_URL}?fileName=${fileName}&uploaderName=${this.state.uploaderName}`, {method: "get"})
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+
+        return response.json()
+      })
+      .then(function(res) {
+        return res.url
+      })
+  },
+
+  findInvalidFiles: function(files) {
+    return Array.prototype.filter.call(files, function(file) {
+      return file.type !== "image/jpeg"
+    })
+  },
+
+  uploadFile: function(file) {
+    const toUpdate = this.state.uploads || {}
+    toUpdate[file.name] = "uploading"
+    this.setState({uploads: toUpdate})
+
+    return this.getPreSignedUrl(file)
+      .then(function(url) {
+        return new Promise(function(resolve, reject) {
+          const xhr = new XMLHttpRequest()
+
+          xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+              if (xhr.status === 200) {
+                const toUpdate = this.state.uploads
+                toUpdate[file.name] = "complete"
+                this.setState({uploads: toUpdate})
+                resolve({})
+              } else {
+                reject({
+                  status: xhr.status,
+                  statusText: xhr.statusText,
+                })
+              }
+            }
+          }.bind(this)
+
+          xhr.open("PUT", url)
+          xhr.send(file)
+        }.bind(this))
+      }.bind(this))
+  },
+
+  onDrop: function(e) {
+    this.onDragLeave(e)
+
+    if (!this.state.uploaderName.length) {
+      this.setState({error: "Please provide an author name"})
+      return
+    }
+
+    const files = e.dataTransfer.files
+    const invalidFiles = this.findInvalidFiles(files)
+
+    if (invalidFiles.length) {
+      this.setState({error: "You can only upload .jpg images"})
+      return
+    }
+
+    Array.prototype.forEach.call(files, function(file) {
+      this.uploadFile(file)
+    }.bind(this))
+  },
+
+  render: function() {
+    const className = (this.state.hover) ? "drop-area hover" : "drop-area"
+
+    return (
+      <div>
+        <h1>Upload Your Photos</h1>
+        {this.state.error && <p className="error">{this.state.error}</p>}
+        {!this.state.uploads && <div>
+          <input value={this.state.uploaderName} onChange={this.onInputChange} placeholder="Add your name here" />
+          <div className={className} onDragEnter={this.onDragEnter} onDragLeave={this.onDragLeave} onDragOver={this.onDragEnter} onDrop={this.onDrop}>
+            <div>
+              <h3>Drag and drop your images here</h3>
+            </div>
+          </div>
+        </div>}
+        {this.state.uploads && <div id="progress">
+          {JSON.stringify(this.state.uploads)}
+        </div>}
+      </div>
+    )
+  },
+})
+
+module.exports = Upload
